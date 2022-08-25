a895fb67002ef661bc45678df1595c1bcd458fab
Alexey-Kushnir <al7787@gmail.com>
2022-08-25 18:59:18 +0300

    9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.body;
+const button = document.querySelector('.change-color');
+
+button.addEventListener(
+  'click',
+  () => (body.style.backgroundColor = getRandomHexColor())
+);
